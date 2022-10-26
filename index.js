8c536164436ff1661a1ec4e37532fd7f038818e3
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-10-25 18:34:27 -0700

    added functions to prompt adding a new member or end after the manager inputs their info

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //packages needed for this application
 const inquirer = require('inquirer');
+const Manager = require('./manager');
+const Engineer = require('./engineer');
+const Intern = require('./intern');
 const fs = require("fs");
-const util = require('util');
 
-const generateReadme = require("./utils/generateHtml");
-const writeFileAsync = util.promisify(fs.writeFile);
+const generateHtml = require("./generateHtml.js");
 
+const teamMembers = [];
 //--------manager section---------
 const questions = [ 
 {
@@ -26,70 +28,94 @@ const questions = [
 {
     type: 'input',
     name: 'managersOfficeNumber',
-    message: 'what is your managers office number',
-},
-{
-    type: 'list',
-    name: 'teamMemberAdd',
-    message: 'what type of team member would you like to add?',
-    choices: ['engineer','intern', 'finish building my team'],
-},
-//present questions below based off selection above
-
-//-------engineer section------------
-{
-    type: 'input',
-    name: 'engineerName',
-    message: 'what is your engineers name?',
-},
-{
-    type: 'input',
-    name: 'engineerId',
-    meessage: 'what is your engineers id?'
-},
-{
-    type: 'input',
-    name: 'engineerEmail',
-    message: 'what is your engineer email?'
-},
-{
-    type: 'input',
-    name: 'engineerGitHub',
-    message: 'what is your engineer Github username?'
-},
-//----------intern section--------------
-{
-    type: 'input',
-    name: 'internName',
-    message: 'What is your interns name?'
-},
-{
-    type: 'input',
-    name: 'internId',
-    message: 'what is your interns ID?'
-},
-{
-    type: 'input',
-    name: 'internEmail',
-    message: 'what is your interns email?'
+    message: 'what is your managers office number?',
 },];
+//grabs manger inputs 
+function managerInfo() {
+inquirer.prompt(questions)
+.then(answer => {
+const newManager = new Manager(answer.managerName, answer.managerId, answer.managerEmail, answer.managerOfficeNumber);
+teamMembers.push(newManager);
+addTeamMember();
+})
+};
+//prompts user to choose a team member or finish the application
+function addTeamMember() {
+ inquirer.prompt([
+    {
+        type: 'list',
+        name: 'teamMemberAdd',
+        message: 'what type of team member would you like to add?',
+        choices: ['engineer','intern', 'finish building my team'],
+    },
+])
 
-//function to write index.html file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName,generateHtml(data), function(err) {
-      if (err) {
-          return console.log(err);
-      }
-  });
-  
-  }
-//uses inquirer to fill in answers to new html
-function init() {
-inquirer
-    .prompt(questions)
-    .then((answers) => {
-    writeToFile("newindex.html", answers);
-});
-}
-// Function call
-init();
+//switch case to prompt each inquirer for each selection and adds to addTeamMember
+.then(answer => {
+    switch(answer.teamMemberAdd){
+        case "engineer":
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: 'what is your engineers name?',
+            },
+            {
+                type: 'input',
+                name: 'engineerId',
+                meessage: 'what is your engineers id?'
+            },
+            {
+                type: 'input',
+                name: 'engineerEmail',
+                message: 'what is your engineer email?'
+            },
+            {
+                type: 'input',
+                name: 'engineerGitHub',
+                message: 'what is your engineer Github username?'
+            },
+        ])
+        .then(answer => {
+            const newEngineer = new Engineer(answer.engineerName, answer.engineerId, answer.engineerEmail, answer.engineerGitHub);
+            teamMembers.push(newEngineer);
+            addTeamMember();
+        })
+        break;
+        case "intern":
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'internName',
+                    message: 'What is your interns name?'
+                },
+                {
+                    type: 'input',
+                    name: 'internId',
+                    message: 'what is your interns ID?'
+                },
+                {
+                    type: 'input',
+                    name: 'internEmail',
+                    message: 'what is your interns email?'
+                },
+                {
+                    type: 'input',
+                    name: 'school',
+                    message: 'enter interns school'
+                }
+            ])
+            .then(answer => {
+                const newIntern = new Intern(answer.internName, answer.internId, answer.internEmail, answer.school);
+                teamMembers.push(newIntern);
+                addTeamMember();
+            })
+            break;
+            case "finish building my team":
+            fs.writeFileSync("newindex.html", generateHtml(teamMembers), "utf-8")
+            break;
+    }
+})
+};
+//calls manager info function
+managerInfo();
